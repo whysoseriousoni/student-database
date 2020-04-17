ccaced89867a93ff2f4b991303b218ad81c6ecd6
whysoseriousoni <43212184+whysoseriousoni@users.noreply.github.com>
2020-04-17 18:22:04 +0530

    Add files via upload

---
+++ src/UI/Declare_path.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author WhysoseriousONI
+ */
+public class Declare_path  {
+    public static final String DBU_STRING = "jdbc:ucanaccess://C:\\Users\\WhysoseriousONI\\Desktop\\studentdb\\student.accdb";
+    //change WhysoseriousONI to your user name
+
+}
